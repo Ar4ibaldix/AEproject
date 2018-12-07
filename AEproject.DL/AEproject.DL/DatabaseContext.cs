f8d9188f87bce6a36acd03df70b20294702f9de0
triputko <triput1994@gmail.com>
2018-12-07 11:00:32 +0300

    updated net core and added database start logic (#1)

---
+++ AEproject.DL/AEproject.DL/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿namespace AEproject.DL
+{
+    using Microsoft.EntityFrameworkCore;
+    using Model;
+
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
